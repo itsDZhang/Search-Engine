44b707ba3897390ef839d41f8cfad698e57f0645
ruiqing.zhang18@gmail.com <ruiqing.zhang18@gmail.com>
2018-02-26 19:18:50 -0500

    ap works now

--- src/Evaluation/Evaluate.java
+++ src/Evaluation/Evaluate.java
@@ -16,9 +16,178 @@ public class Evaluate {
 //		Student 6 is bad maybe 10? may 12?
 		
 		calcMeanAveragePrecision(resultFile, qrels);
+		calcPrecisionAt10(resultFile, qrels);
+		
+		calcNdcg10(resultFile, qrels);
+		calcNdcg1000(resultFile, qrels);
 		
 		
 
+	}
+	public static void calcNdcg1000(String resultFile, String qrels) throws Exception {
+		ResultsFile resultsRaw = new ResultsFile(resultFile);
+		qRels qrelsList = new qRels(qrels);
+		
+		Results results = resultsRaw.results;
+		ArrayList<Result> queryResult = new ArrayList<>();
+		ArrayList<String> topicIds = results.QueryIDs();
+		HashMap<String, ArrayList<Result>> query2Result = new HashMap<>();
+		HashMap<String, ArrayList<String>> topic2RelDocnos = new HashMap<>();
+		RelevanceJudgements relDocs = qrelsList.judgements;
+		ArrayList<String> reldocnos = new ArrayList<>();
+		ArrayList<Double> ndcgList = new ArrayList<>();
+		
+		for(String topicNum : topicIds) {
+			queryResult = results.QueryResults(topicNum);
+			query2Result.put(topicNum, queryResult);
+			topic2RelDocnos.put(topicNum, relDocs.getRelDocnos(topicNum));
+		}
+		Collections.sort(topicIds);
+		double ndcg = 0;
+		double idcg = 0;
+		for( int i =1; i <=1000; i++) {
+			idcg += (1/(Math.log(i+1)/Math.log(2)));
+		}
+		
+		for(String topic : topicIds) {
+			int breaker = 0;
+			double dcg = 0;
+			queryResult = query2Result.get(topic);
+			reldocnos = topic2RelDocnos.get(topic);
+			
+			String docno = "";
+			int stop = 1000;
+			if( stop != queryResult.size()) {
+				stop = queryResult.size();
+			}
+			for(int i=1; i<=stop; i++ ) {
+				Result result = queryResult.get(i-1);
+//				System.out.println(result.getRank());
+				docno = result.getDocID();
+				if(reldocnos.contains(docno)) {
+					System.out.println(result.getRank());
+					dcg += (1/(Math.log(result.getRank() + 1)/Math.log(2)));
+					
+				}
+			}
+			
+
+			
+			ndcg = dcg/idcg;
+			ndcgList.add(ndcg);
+			System.out.println("Topic: "+ topic + " dcg: " + dcg +" idcg: " + idcg);
+			System.out.println("NDCG: "+ ndcg);
+			
+		}
+		
+		System.out.println(ndcgList.toString());
+//		for(Result result: queryResult) {
+//		breaker +=1;
+//		docno = result.getDocID();
+//		if(reldocnos.contains(docno)) {
+//			System.out.println(result.getRank());
+//			dcg += (1/(Math.log(result.getRank() + 1)/Math.log(2)));
+//			
+//		}
+//		if(breaker == 1000) {
+//			break;
+//		}
+//		
+//		
+//	}
+	}
+	
+	public static void calcNdcg10(String resultFile, String qrels) throws Exception {
+		ResultsFile resultsRaw = new ResultsFile(resultFile);
+		qRels qrelsList = new qRels(qrels);
+		
+		Results results = resultsRaw.results;
+		ArrayList<Result> queryResult = new ArrayList<>();
+		ArrayList<String> topicIds = results.QueryIDs();
+		HashMap<String, ArrayList<Result>> query2Result = new HashMap<>();
+		HashMap<String, ArrayList<String>> topic2RelDocnos = new HashMap<>();
+		RelevanceJudgements relDocs = qrelsList.judgements;
+		ArrayList<String> reldocnos = new ArrayList<>();
+		ArrayList<Double> ndcgList = new ArrayList<>();
+		for(String topicNum : topicIds) {
+			queryResult = results.QueryResults(topicNum);
+			query2Result.put(topicNum, queryResult);
+			topic2RelDocnos.put(topicNum, relDocs.getRelDocnos(topicNum));
+		}
+		Collections.sort(topicIds);
+		double ndcg = 0;
+		double idcg = 0;
+		for( int i =1; i <=10; i++) {
+			idcg += (1/(Math.log(i+1)/Math.log(2)));
+		}
+		
+		for(String topic : topicIds) {
+			double dcg = 0;
+			queryResult = query2Result.get(topic);
+			reldocnos = topic2RelDocnos.get(topic);
+			
+			String docno = "";
+			
+			for(int breaker=1; breaker<=10; breaker++ ) {
+				Result result = queryResult.get(breaker-1);
+//				System.out.println(result.getRank());
+				docno = result.getDocID();
+				if(reldocnos.contains(docno)) {
+//					System.out.println(result.getRank());
+					dcg += (1/(Math.log(result.getRank() + 1)/Math.log(2)));
+					
+				}
+			}
+			ndcg = dcg/idcg;
+			ndcgList.add(ndcg);
+//			System.out.println("Topic: "+ topic + " dcg: " + dcg +" idcg: " + idcg);
+//			System.out.println("NDCG: "+ ndcg);
+			
+			
+			
+		}
+		
+		System.out.println(ndcgList.toString());
+		
+	}
+	
+	public static void calcPrecisionAt10(String resultFile, String qrels) throws Exception {
+		ResultsFile resultsRaw = new ResultsFile(resultFile);
+		Results results = resultsRaw.results;
+		HashMap<String, ArrayList<Result>> query2Result = new HashMap<>();
+		ArrayList<Result> queryResult = new ArrayList<>();
+		ArrayList<String> topicIds = results.QueryIDs();
+		HashMap<String, ArrayList<String>> topic2RelDocnos = new HashMap<>();
+		ArrayList<Double> avgScores = new ArrayList<>();
+		qRels qrelsList = new qRels(qrels);
+		RelevanceJudgements relDocs = qrelsList.judgements;
+		ArrayList<String> reldocnos = new ArrayList<>();
+		for(String topicNum : topicIds) {
+			queryResult = results.QueryResults(topicNum);
+			query2Result.put(topicNum, queryResult);
+			topic2RelDocnos.put(topicNum, relDocs.getRelDocnos(topicNum));
+		}
+		Collections.sort(topicIds);
+		
+		for(String topic : topicIds) {
+			queryResult = query2Result.get(topic);
+			reldocnos = topic2RelDocnos.get(topic);
+			double precision = 0.0;
+			double counter = 0.0;
+			double rank = 10.0;
+			String docno = "";
+			
+			for(int breaker=0; breaker<10; breaker++ ) {
+				Result result = queryResult.get(breaker);
+				docno = result.getDocID();
+				if(reldocnos.contains(docno)) {
+					counter +=1;
+				}
+			}
+			precision = counter/rank;
+			avgScores.add(precision);
+		}
+		System.out.println(avgScores.toString());
 	}
 	
 	public static void calcMeanAveragePrecision(String resultFile, String qrels) throws Exception {
@@ -32,17 +201,11 @@ public class Evaluate {
 		qRels qrelsList = new qRels(qrels);
 		RelevanceJudgements relDocs = qrelsList.judgements;
 		ArrayList<String> reldocnos = new ArrayList<>();
-		double avg4topicNum = 0;
-		double numerator = 0;
-		double denominator = 0;
-		
 		for(String topicNum : topicIds) {
 			queryResult = results.QueryResults(topicNum);
 			query2Result.put(topicNum, queryResult);
 			topic2RelDocnos.put(topicNum, relDocs.getRelDocnos(topicNum));
 		}
-		
-		
 		Collections.sort(topicIds);
 //		https://people.cs.umass.edu/~jpjiang/cs646/03_eval_basics.pdf
 		for(String topic : topicIds) {
@@ -50,10 +213,10 @@ public class Evaluate {
 			queryResult = query2Result.get(topic);
 //			Array of rel doc nos -- Testing this
 			reldocnos = topic2RelDocnos.get(topic);
-			Double precision = 0.0;
-			Double counter = 0.0;
-			Double rank = 0.0;
-			Double sum = 0.0;
+			double precision = 0.0;
+			double counter = 0.0;
+			double rank = 0.0;
+			double sum = 0.0;
 			String docno = "";
 			
 			for( Result result : queryResult) {
@@ -67,35 +230,13 @@ public class Evaluate {
 					sum += precision;
 				}
 			}
-			System.out.println( "Topic #: " + topic +
-					" |count size: " + counter 
-					+ " |reldocnos size: " + reldocnos.size() +
-					" |queryResult size: " + queryResult.size());
-//			System.out.println("Sum: " + sum);
+//			System.out.println( "Topic #: " + topic +
+//					" |count size: " + counter 
+//					+ " |reldocnos size: " + reldocnos.size() +
+//					" |queryResult size: " + queryResult.size());
 			avgScores.add(sum/reldocnos.size());
-//			avgScores.add(sum/counter);
-			
-			
-			
-			
-			
-			
-			
-//			avg4topicNum = 0;
-			
-//			denominator = topic2RelDocnos.get(topic).size();
-//			numerator = queryResult.size();
-//			avg4topicNum = numerator/denominator;
-//			
-//			avg4topicNum = queryResult.size();
-//			
-//			avgScores.add(avg4topicNum);
 		}
-		
 		System.out.println(avgScores.toString());
-		
-//		==============================================================================================
-		
 	}
 
 }
